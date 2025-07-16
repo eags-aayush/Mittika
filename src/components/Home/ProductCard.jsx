82846ea244eed8bbc5ead4f772d96db8ce758186
Aayush Laddha <aayushladdha001@gmail.com>
2025-07-16 17:22:23 +0530

    .

--- src/components/Home/ProductCard.jsx
+++ src/components/Home/ProductCard.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const ProductCard = ({ product }) => {
   return (
     <div className="bg-gray-100 rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300">
-      <Link to={`/${product.type}/${product.id}`}>
+      {/* <Link to={`/${product.type}/${product.id}`}>
         <img src={product.imageUrl} alt={product.name} className="w-full h-64 object-cover" />
-      </Link>
+      </Link> */}
       <div className="p-4">
         <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
         <p className="text-gray-700 mb-2">{product.description}</p>
